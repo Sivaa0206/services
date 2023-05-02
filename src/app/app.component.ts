f336d53d5f689bc7bb26435b692abdc570077a9f
Sivaa0206 <vinsmokesanji020620@gmail.com>
2023-05-02 10:48:45 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+ 
+import { ProductService } from './product.service';
+import { Product } from './product';
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'services';
+ 
+export class AppComponent
+{
+ 
+   products:Product[] | undefined;
+   productService;
+ 
+   constructor(){
+     this.productService=new ProductService();
+   }
+ 
+   getProducts() {
+     
+     this.products=this.productService.getProducts();
+   }
+  
 }
